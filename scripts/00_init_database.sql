7b19d11b4d040f6b17ab04b9dbd40bb30d22d0fe
martynawieruszewska <martyna.wieruszewska@gmail.com>
2026-08-17 10:15:50 +0200

    initial database

--- scripts/00_init_database.sql
+++ scripts/00_init_database.sql
@@ -1,114 +1,93 @@
 /*
 =============================================================
-Create Database and Schemas
+Create Database
 =============================================================
 Script Purpose:
-    This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, this script creates a schema called gold
-	
+    This script creates a new database named DataWarehouseAnalytics.
+
 WARNING:
-    Running this script will drop the entire 'DataWarehouseAnalytics' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
+    If the database already exists, it will be dropped.
+    All data inside it will be permanently deleted.
 */
 
-USE master;
-GO
+DROP DATABASE IF EXISTS "DataWarehouseAnalytics";
 
--- Drop and recreate the 'DataWarehouseAnalytics' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouseAnalytics')
-BEGIN
-    ALTER DATABASE DataWarehouseAnalytics SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouseAnalytics;
-END;
-GO
+CREATE DATABASE "DataWarehouseAnalytics"; 
+/*
+=============================================================
+Create Schema and Tables
+=============================================================
+*/
 
--- Create the 'DataWarehouseAnalytics' database
-CREATE DATABASE DataWarehouseAnalytics;
-GO
-
-USE DataWarehouseAnalytics;
-GO
-
--- Create Schemas
+-- Drop and recreate schema
+DROP SCHEMA IF EXISTS gold CASCADE;
 
 CREATE SCHEMA gold;
-GO
 
-CREATE TABLE gold.dim_customers(
-	customer_key int,
-	customer_id int,
-	customer_number nvarchar(50),
-	first_name nvarchar(50),
-	last_name nvarchar(50),
-	country nvarchar(50),
-	marital_status nvarchar(50),
-	gender nvarchar(50),
-	birthdate date,
-	create_date date
-);
-GO
 
-CREATE TABLE gold.dim_products(
-	product_key int ,
-	product_id int ,
-	product_number nvarchar(50) ,
-	product_name nvarchar(50) ,
-	category_id nvarchar(50) ,
-	category nvarchar(50) ,
-	subcategory nvarchar(50) ,
-	maintenance nvarchar(50) ,
-	cost int,
-	product_line nvarchar(50),
-	start_date date 
-);
-GO
+-- ============================================================
+-- Create dim_customers
+-- ============================================================
 
-CREATE TABLE gold.fact_sales(
-	order_number nvarchar(50),
-	product_key int,
-	customer_key int,
-	order_date date,
-	shipping_date date,
-	due_date date,
-	sales_amount int,
-	quantity tinyint,
-	price int 
+CREATE TABLE gold.dim_customers (
+    customer_key      INTEGER,
+    customer_id       INTEGER,
+    customer_number   VARCHAR(50),
+    first_name        VARCHAR(50),
+    last_name         VARCHAR(50),
+    country           VARCHAR(50),
+    marital_status    VARCHAR(50),
+    gender            VARCHAR(50),
+    birthdate         DATE,
+    create_date       DATE
 );
-GO
+
+
+-- ============================================================
+-- Create dim_products
+-- ============================================================
+
+CREATE TABLE gold.dim_products (
+    product_key       INTEGER,
+    product_id        INTEGER,
+    product_number    VARCHAR(50),
+    product_name      VARCHAR(50),
+    category_id       VARCHAR(50),
+    category          VARCHAR(50),
+    subcategory       VARCHAR(50),
+    maintenance       VARCHAR(50),
+    cost              INTEGER,
+    product_line      VARCHAR(50),
+    start_date        DATE
+);
+
+
+-- ============================================================
+-- Create fact_sales
+-- ============================================================
+
+CREATE TABLE gold.fact_sales (
+    order_number      VARCHAR(50),
+    product_key       INTEGER,
+    customer_key      INTEGER,
+    order_date        DATE,
+    shipping_date     DATE,
+    due_date          DATE,
+    sales_amount      INTEGER,
+    quantity          SMALLINT,
+    price             INTEGER
+);
 
 TRUNCATE TABLE gold.dim_customers;
-GO
 
-BULK INSERT gold.dim_customers
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.dim_customers.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
+\copy gold.dim_customers FROM '/Users/martyna/Documents/sql/sql-data-analytics-project/datasets/flat-files/dim_customers.csv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
+
 
 TRUNCATE TABLE gold.dim_products;
-GO
 
-BULK INSERT gold.dim_products
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.dim_products.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
+\copy gold.dim_products FROM '/Users/martyna/Documents/sql/sql-data-analytics-project/datasets/flat-files/dim_products.csv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
+
 
 TRUNCATE TABLE gold.fact_sales;
-GO
 
-BULK INSERT gold.fact_sales
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.fact_sales.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
+\copy gold.fact_sales FROM '/Users/martyna/Documents/sql/sql-data-analytics-project/datasets/flat-files/fact_sales.csv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
